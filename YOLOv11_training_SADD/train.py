f9e7b87ee3c7f086801203b435c87d8809c6daae
T.J. Rizak <tjriz@udel.edu>
2024-11-05 11:21:35 -0500

    Generated files after training run with 100 epochs for both original 224x224 slices and resized to 200x200 slices (same randomized 70-20-10 split), and also the version of the dataset which was preprocessed and resized to 640x640 (internet resources indicate this to be a typical input size for YOLOv11 architecture)

--- YOLOv11_training_SADD/train.py
+++ YOLOv11_training_SADD/train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # Define paths and configurations
 model_path = 'yolo11m.pt'
-data_yaml = '/home/tjriz/Documents/Senior-Design/datasets/SADD/hust-rslab-SAR-aircraft-data.v2i.yolov11.resize200/data.yaml'  # Path to the updated data.yaml file
+data_yaml = '/home/tjriz/Documents/Senior-Design/datasets/SADD/hust-rslab-SAR-aircraft-data.v2i.yolov11.newsplit/data.yaml'  # Path to the updated data.yaml file
 results_dir = '/home/tjriz/Documents/Senior-Design/YOLOv11_training_SADD/results'  # Directory to save results
 
 # Initialize YOLO model with pre-trained weights
@@ -13,7 +13,7 @@ model.train(
     data=data_yaml,           # Path to the YAML configuration file
     epochs=100,                # Number of training epochs (adjust based on need)
     batch=16,                  # Batch size (adjust based on GPU memory)
-    imgsz=200,                 # Input image size (matches the preprocessed size)
+    imgsz=224,                 # Input image size (matches the preprocessed size)
     save_dir=results_dir,      # Directory to save training logs and checkpoints
     name='airplane_detection', # Custom name for the experiment
     device=0                   # Specifies GPU (0) or CPU (-1)
