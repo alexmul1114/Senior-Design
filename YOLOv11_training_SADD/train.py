2510dac61fb915354368cf844722494cdc767aa6
T.J. Rizak <tjriz@udel.edu>
2024-11-10 17:36:14 -0500

    After creating code for repeatable 'randomized' split, ran a second training run, for all three image sizes, this time with 200 epochs each. It appears that this led to better results. However, the 640x640 dataset used thus far is not the same as the current split for the 200 and 224 sized datasets. Will update this.

--- YOLOv11_training_SADD/train.py
+++ YOLOv11_training_SADD/train.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 
 # Define paths and configurations
 model_path = 'yolo11m.pt'
-data_yaml = '/home/tjriz/Documents/Senior-Design/datasets/SADD/hust-rslab-SAR-aircraft-data.v2i.yolov11.newsplit/data.yaml'  # Path to the updated data.yaml file
+data_yaml = '/home/tjriz/Documents/Senior-Design/datasets/SADD/hust-rslab-SAR-aircraft-data.v1i.yolov11/data.yaml'  # Path to the updated data.yaml file
 results_dir = '/home/tjriz/Documents/Senior-Design/YOLOv11_training_SADD/results'  # Directory to save results
 
 # Initialize YOLO model with pre-trained weights
@@ -14,11 +14,11 @@ model = YOLO(model_path)
 # Set training configurations and start training
 model.train(
     data=data_yaml,           # Path to the YAML configuration file
-    epochs=100,                # Number of training epochs (adjust based on need)
-    batch=16,                  # Batch size (adjust based on GPU memory)
-    imgsz=224,                 # Input image size (matches the preprocessed size)
+    epochs=200,                # Number of training epochs (adjust based on need)
+    batch=12,                  # Batch size (adjust based on GPU memory)
+    imgsz=640,                 # Input image size (matches the preprocessed size)
     save_dir=results_dir,      # Directory to save training logs and checkpoints
-    name='airplane_detection', # Custom name for the experiment
+    name='airplane_detection_resize640_2', # Custom name for the experiment
     device=0                   # Specifies GPU (0) or CPU (-1)
 )
 
